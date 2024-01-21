8a86a8f08eab391245b435b25f7fcebc6ae73fea
midhunhk <2026817+midhunhk@users.noreply.github.com>
2024-01-21 00:17:06 -0500

    update build out directory

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  // https://vitejs.dev/config/build-options.html#build-outdir
+  build:{
+    outDir:'docs'
+  }
 })
